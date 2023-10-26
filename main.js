2282aa2a0e38331afbc5502ff65cb8744fa64954
saranatour1 <saranatourcontact@gmail.com>
2023-10-27 01:45:34 +0300

    movement and collision detection

--- main.js
+++ main.js
@@ -5,12 +5,22 @@ import jump from "./assets/jump.png";
 import move from "./assets/move.png";
 import run from "./assets/run.png";
 import jumpStart from "./assets/jump-start.png";
+import obstacle1 from "./assets/obstacle1.png";
+import obstacle2 from "./assets/obstacle2.png";
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 let xCenter = Math.floor(canvas?.width / 2);
 let yCenter = Math.floor(canvas?.height / 2);
+
+let lives = 3;
+let collisionDetected = false;
+
+const camera = {
+  x: 0,
+  y: 0,
+};
 
 const moveKey = {
   up: "ArrowUp",
@@ -45,6 +55,13 @@ function resizeCanvas() {
   yCenter = Math.floor(canvas?.height / 2);
 }
 
+const obstacles = [
+  { x: xCenter + 700, y: yCenter + 290, image: obstacle1 },
+  { x: xCenter + 708, y: yCenter + 290, image: obstacle2 },
+  { x: xCenter + 668, y: yCenter + 290, image: obstacle1 },
+  { x: xCenter + 568, y: yCenter + 290, image: obstacle2 },
+];
+
 if (canvas) {
   resizeCanvas();
   window.addEventListener("resize", () => {
@@ -56,8 +73,18 @@ function clearCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
+function drawLine() {
+  const lineY = yCenter + Math.floor(currentImage.height / 2) + 18;
+  // Draw the line
+  ctx.beginPath();
+  ctx.moveTo(0, lineY);
+  ctx.lineTo(canvas.width, lineY);
+  ctx.stroke();
+}
+
 function drawImageActualSize() {
   clearCanvas();
+  drawLine();
   ctx.drawImage(
     currentImage,
     xCenter,
@@ -67,9 +94,60 @@ function drawImageActualSize() {
   );
 }
 
+function drawObstacles() {
+  for (const obstacle of obstacles) {
+    const obstacleImage = new Image(64, 64);
+    obstacleImage.src = obstacle.image;
+    ctx.drawImage(obstacleImage, obstacle.x, obstacle.y, 64, 64);
+  }
+}
+
+function moveObstacles() {
+  for (const obstacle of obstacles) {
+    obstacle.x -= 5;
+    if (obstacle.x <= 0) {
+      obstacle.x = canvas?.width + Math.floor(Math.round(Math.random()) * 100);
+    }
+  }
+}
+
 function switchImage(imageKey) {
   currentImage = images[imageKey];
   drawImageActualSize();
+}
+
+function collisionDetection() {
+  if (!collisionDetected) {
+    for (const obstacle of obstacles) {
+      const obstacleRight = obstacle.x + 64;
+      const obstacleBottom = obstacle.y + 64;
+      const characterRight = xCenter + currentImage.width / 2;
+      const characterBottom = yCenter + currentImage.height / 2;
+
+      if (
+        xCenter - currentImage.width / 2 < obstacleRight &&
+        characterRight > obstacle.x &&
+        yCenter - currentImage.height / 2 < obstacleBottom &&
+        characterBottom > obstacle.y
+      ) {
+        console.log("collision");
+        lives--; 
+        collisionDetected = true;
+      }
+    }
+  }
+}
+
+function resetCollision() {
+
+} 
+
+
+
+function drawText() {
+  ctx.font = "48px serif";
+  ctx.fillText(`${camera.x}`, 10, 50);
+  ctx.fillText(`Lives: ${lives}`, canvas?.width - 200, 50);
 }
 
 // shortcut keys
@@ -84,26 +162,31 @@ document.addEventListener("keydown", (e) => {
     currentImageKey = "move";
   }
   if (e.key === moveKey.right || e.key === "d") {
+    camera.x += 5;
     if (currentImageKey === "run") {
       currentImageKey = "move";
     } else {
       currentImageKey = "run";
     }
+    moveObstacles();
   }
   if (e.key === moveKey.jump || e.key === " ") {
-    console.log("jump");
     currentImageKey = "jump";
   }
 });
 
 document.addEventListener("keyup", (e) => {
-  console.log(e.key, "keyup");
   currentImageKey = "front";
-
+  // resetCollision();
 });
 
 function animate() {
+
   switchImage(currentImageKey);
+
+  drawObstacles();
+  collisionDetection();
+  drawText();
   requestAnimationFrame(animate);
 }
 
